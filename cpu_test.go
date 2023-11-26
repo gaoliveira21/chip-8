c9c5beca575ee76f2066de84815215ba1985df5f
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-11-26 09:09:22 -0300

    test(cpu): add Decode test

--- cpu_test.go
+++ cpu_test.go
@@ -25,3 +25,40 @@ func TestNewCpu(t *testing.T) {
 		t.Error("Error loading fonts")
 	}
 }
+
+func TestDecode(t *testing.T) {
+	cpu := NewCpu()
+
+	opcode := cpu.Decode(0xABCD)
+
+	var expected uint16 = 0xA000
+
+	if opcode.instruction != expected {
+		t.Errorf("opcode.instruction = 0x%X; expected 0x%X", opcode.instruction, expected)
+	}
+
+	expected = 0xB
+	if opcode.registerX != uint8(expected) {
+		t.Errorf("opcode.registerX = 0x%X; expected 0x%X", opcode.registerX, expected)
+	}
+
+	expected = 0xC
+	if opcode.registerY != uint8(expected) {
+		t.Errorf("opcode.registerY = 0x%X; expected 0x%X", opcode.registerY, expected)
+	}
+
+	expected = 0xD
+	if opcode.n != uint8(expected) {
+		t.Errorf("opcode.n = 0x%X; expected 0x%X", opcode.n, expected)
+	}
+
+	expected = 0xCD
+	if opcode.nn != uint8(expected) {
+		t.Errorf("opcode.n = 0x%X; expected 0x%X", opcode.nn, expected)
+	}
+
+	expected = 0xBCD
+	if opcode.nnn != expected {
+		t.Errorf("opcode.n = 0x%X; expected 0x%X", opcode.nnn, expected)
+	}
+}
