b46486e14ec9f9a137ada661cd1f15cf52bec476
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-11-26 08:10:31 -0300

    test(mmu): add MMU test

---
+++ memory/mmu_test.go
@@ -0,0 +1,28 @@
+package memory_test
+
+import (
+	"testing"
+
+	"github.com/gaoliveira21/chip8/memory"
+)
+
+func TestMMU(t *testing.T) {
+	mmu := new(memory.MMU)
+
+	mmu.Write(0x00, 0x00)
+	mmu.Write(0x01, 0xEE)
+
+	mmu.Write(0xFFE, 0xAA)
+	mmu.Write(0xFFF, 0xBB)
+
+	word1 := mmu.Fetch(0x00)
+	word2 := mmu.Fetch(0xFFE)
+
+	if word1 != 0x00EE {
+		t.Errorf("Fetch(0x00) = %d; expected 0x00EE", word1)
+	}
+
+	if word2 != 0xAABB {
+		t.Errorf("Fetch(0x00) = %d; expected 0xAABB", word2)
+	}
+}
