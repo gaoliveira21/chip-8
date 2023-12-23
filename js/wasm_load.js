87a31f8dacf35450cf92c72dd0539c7ab355cf0c
gaoliveira21 <gaoliveira21@users.noreply.github.com>
2023-12-23 14:20:08 +0000

    deploy: 87989e706f65d84c4e5032f30790e4c86e533910

--- js/wasm_load.js
+++ js/wasm_load.js
@@ -1,5 +1,29 @@
-const go = new Go()
+const form = document.querySelector("form#select-rom")
 
-WebAssembly
-  .instantiateStreaming(fetch("chip8.wasm"), go.importObject)
-  .then(result => go.run(result.instance))
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const data = new FormData(form)
+
+  const rom = data.get("rom")
+
+  document.rom = rom
+
+  form.remove()
+
+  const go = new Go()
+
+  WebAssembly
+    .instantiateStreaming(fetch(`chip8.wasm`), go.importObject)
+    .then(result => {
+      go.run(result.instance)
+      const canvas = document.getElementsByTagName("canvas")[0]
+
+      canvas.addEventListener("keydown", (e) => {
+        console.log(e.key)
+          if (e.key == "Escape") {
+            location.reload()
+          }
+        })
+    })
+})
