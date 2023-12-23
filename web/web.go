1de33915e3d21fe377e6a84247e1678f2300f67b
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-12-23 11:17:25 -0300

    feat(web): add basic html structure

--- web/web.go
+++ web/web.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"path"
+	"syscall/js"
 
 	"github.com/gaoliveira21/chip8/core"
 	"github.com/gaoliveira21/chip8/web/http"
@@ -11,11 +13,15 @@ import (
 func main() {
 	os.Setenv("EXEC_MODE", "web")
 
-	rom, err := http.ReadFile("roms/PONG.ch8")
+	document := js.Global().Get("document")
+
+	romName := document.Get("rom").String()
+
+	rom, err := http.ReadFile(path.Join("roms", romName))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	core.RunChip8(rom, "[CHIP-8] - PONG")
+	core.RunChip8(rom, "[CHIP-8] - "+romName)
 }
